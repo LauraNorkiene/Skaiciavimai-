17c3b5159458f74247958136c410f07073979d2a
unknown <laura@gmail.com>
2022-08-23 09:01:07 +0300

    commit

--- nav.php
+++ nav.php
@@ -46,9 +46,30 @@
             </div>
          </div>
       </nav>
+   </div>
 
+   <div class="card mx-auto mt-5 border border-dark" style="width: 55rem;">
+      <div class="card-body mx-auto">
+         <h5 class="card-title">Namų darbai</h5>
 
+         <p>Sukurkite puslapius į kurios galėtumėme įvesti duomenis, ir gautumėme atsakymą (kiekviena užduotis turi būti sprendžiama skirtingame puslapyje, padarykite patogų vaikščiojimą tarp užduočių (meniu)).:</p>
 
+         <p> Įvedami skaičiai -a, b, c –kraštinių ilgiai (į atskirus input laukus). Parašykite PHP programą, kuri nustatytų, ar galima sudaryti trikampį ir rezultatą išvestų. </p>
+
+         <p> 2. Pirmosios vasaros olimpinės žaidynės įvyko 1896 m. Atėnuose. Po to jos vyko arba turėjo vykti kas ketveri metai, t.y. 1900 m. – antrosios, 1904 m. – trečiosios ir t.t. Neįvykusioms žaidynėms skiriamas eilės numeris, o jų metai vis tiek laikomi olimpiniais. Tinklapyje turi būti įvedami metai, nustatykite olimpinių žaidimų numerį, jei metai yra olimpiniai arba praneškite, kad metai ne olimpiniai. Tinkalpyje turi būti išvedama ar tai buvo olimpiniai metai ar ne, jei tai buvo olimpiniai metai taip pat išvesti ir jų numerį.</p>
+
+         <p> 3. Senovės japonų kalendorių sudarė 60 metų ciklas. Visi metai cikle buvo sunumeruoti nuo 1 iki 60 ir suskirstyti poromis, kurių kiekviena turėjo savo spalvą (žalią, raudoną, geltoną, baltą ar juodą). Ciklo metų spalvos buvo paskirstytos taip:
+            1, 2, 11, 12, 21, 22, …, 51, 52 metai – žalia spalva;
+            3, 4, 13, 14, 23, 24, …, 53, 54 metai – raudona spalva;
+            5, 6, 15, 16, 25, 26, …, 55, 56 metai – geltona spalva;
+            7, 8, 17, 18, 27, 28, …, 57, 58 metai – balta spalva;
+            9, 10, 19, 20, 29, 30, …, 59, 60 metai – juoda spalva.</p>
+
+         <p> Žinoma, kad naujasis 60 metų ciklas prasidėjo 1984-aisiais ir baigsis 2043-iaisiais metais; 1984-ieji ir 1985-ieji buvo žalios spalvos metai, 1986-ieji ir 1987-ieji buvo raudonos spalvos metai, 2043-ieji bus juodos spalvos metai. Sukurkite puslapį kuriame būtų įvedami metai ir būtų išvedama kokia yra įvestų metų spalva pagal japonų kalendorių.</p>
+
+         <p> Sprendžiant užduotį panaudokite switch sąkinį.
+         </p>
+      </div>
    </div>
 </body>
 
